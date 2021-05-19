97af8be59b3c8b88ae513c6780fc9cc5eead7b61
Sahil <sahiil0710@gmail.com>
2021-05-19 11:33:33 -0400

    overhaul

--- src/main/java/com/example/sahil/auth/Application.java
+++ src/main/java/com/example/sahil/auth/Application.java
@@ -1,4 +1,4 @@
-package com.example.sahil.auth.basic;
+package com.example.sahil.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
